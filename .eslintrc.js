94f9a4c80d1c99f4d113e97a9fd9f9d531fc1ac9
Pedro Javier Nicolás Zamora <pedro@zapp-studio.com>
2020-08-01 14:01:31 +0200

    Init nuxt project

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always',
+    }],
+  },
+}
